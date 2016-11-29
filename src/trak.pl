9f063043776998385016911de2d62359d9d03169
Alexej Magura <amagura@users.noreply.github.com>
2016-11-29 09:37:06 -0700

    Create trak.pl

---
+++ src/trak.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/env perl
+use v5.10.1;
+use warnings;
+use strict;
+
+# a processor tracker and logger for keeping track of what you do from day to day
+# tracks things on a minute by minute basis.  
